d8bdf381d241b1f3f64a9b1ac5188f4d0491a92d
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-06 22:36:39 +0000

    Refractored code with a .forEach instead of a for loop

--- findingWaldo.js
+++ findingWaldo.js
@@ -1,17 +1,26 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      found(i);   // execute callback
-    }
+  function waldoLocal(currrentValue, index){
+  if (currrentValue === "Waldo"){
+    found(index);
   }
 }
-
-function actionWhenFound(index) { // functions can take different kinds of parameters incl index!
+arr.forEach(waldoLocal);
+}
+function actionWhenFound(index){
   console.log(index);
 }
-
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+//console.log(findWaldo["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 //calling 2 functions
 //1) findoWaldo with an array and a found.
 //2) calling another function called actionWhenFound.
+
+//Refactor the function findWaldo to use the forEach() method instead of a for loop.
+// var clothes = ["jeans","capris","skinnys","dresses"];
+
+// function fashionable(item, index, array){ // function that makes things fashionale
+// console.log(item + " are fashionable");
+// }
+
+// clothes.forEach(fashionable)
